0b1107aba96e527519fc6ec212ac3174e7be4f94
Yi Zhou <kickccat@hotmail.com>
2018-07-27 16:54:03 +0200

    add web devtools, thymeleaf

---
+++ guess-the-number-game/web/src/main/java/io/sample/util/ViewNames.java
@@ -0,0 +1,10 @@
+package io.sample.util;
+
+public final class ViewNames {
+    
+    // == constants ==
+    public static final String HOME = "home";
+    
+    // == constructor ==
+    private ViewNames() {}
+}
