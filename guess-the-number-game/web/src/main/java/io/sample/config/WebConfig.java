23a1447692be7e9f60b6247b9c6dcfbf7bf5ace0
Yi Zhou <kickccat@hotmail.com>
2018-08-01 10:21:16 +0200

    add Interceptor

--- guess-the-number-game/web/src/main/java/io/sample/config/WebConfig.java
+++ guess-the-number-game/web/src/main/java/io/sample/config/WebConfig.java
@@ -4,15 +4,35 @@ import io.sample.Game;
 import io.sample.GameImpl;
 import io.sample.GuessCount;
 import io.sample.NumberGenerator;
+import io.sample.interceptor.RequestInterceptor;
 import io.sample.util.ViewNames;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.annotation.SessionScope;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    
+    // Bean methods
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInterceptor());
+    
+//        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+//        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(new LocaleChangeInterceptor());
+    }
     
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
