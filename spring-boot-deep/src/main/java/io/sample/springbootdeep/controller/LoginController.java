956413d723db71aea858b47438af737e606e978d
Yi Zhou <kickccat@hotmail.com>
2018-08-23 16:05:44 +0200

    add error information

--- spring-boot-deep/src/main/java/io/sample/springbootdeep/controller/LoginController.java
+++ spring-boot-deep/src/main/java/io/sample/springbootdeep/controller/LoginController.java
@@ -5,13 +5,12 @@ import io.sample.springbootdeep.domain.UserRepository;
 import io.sample.springbootdeep.form.UserForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 public class LoginController {
@@ -24,7 +23,8 @@ public class LoginController {
     }
     
     @GetMapping("/register")
-    public String register() {
+    public String register(Model model) {
+        model.addAttribute("userForm", new UserForm());
         return "register";
     }
     
@@ -35,21 +35,14 @@ public class LoginController {
     
     @PostMapping("/register")
     public String registerReceiver(@Valid UserForm userForm, BindingResult result) {
-        boolean fulfill = true;
+        
         if (!userForm.identityPassword()) {
             result.rejectValue("confirmPassword", "identity error", "两次密码不一致");
-            fulfill = false;
         }
         if (result.hasErrors()) {
-            List<FieldError> fieldErrors = result.getFieldErrors();
-            for (FieldError error : fieldErrors) {
-                System.out.println(error.getField() + " : " + error.getDefaultMessage() + " : " + error.getCode());
-            }
-            fulfill = false;
-        }
-        if (!fulfill) {
             return "register";
         }
+        
         User user = userForm.convertToUser();
         userRepository.save(user);
         return "redirect:/login";
--- spring-boot-deep/src/main/java/io/sample/springbootdeep/form/UserForm.java
+++ spring-boot-deep/src/main/java/io/sample/springbootdeep/form/UserForm.java
@@ -14,21 +14,20 @@ public class UserForm {
     
     public static final String PHONE_REG = "^((13[0-9])|(15[^4])|(18[0235-9])|(17[0-8])|(147))\\d{8}$";
     
-    @NotBlank
+    @NotBlank(message = "用户名不能为空")
     private String username;
     
-    @NotBlank
     @Length(min = 6, max = 20, message = "密码至少需要6位")
     private String password;
     
-    @NotBlank
+    @NotBlank(message = "密码错误")
     private String confirmPassword;
     
     @Pattern(regexp = PHONE_REG, message = "请输入正确的手机号")
     private String phone;
     
-    @NotBlank
-    @Email
+    @NotBlank(message = "请填写邮箱")
+    @Email(message = "邮箱格式不正确")
     private String email;
     
     public UserForm() {
