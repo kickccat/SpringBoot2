18fb62668056c3adbeed8956a26df9cd18bb2b3c
Yi Zhou <kickccat@hotmail.com>
2018-08-24 15:16:27 +0200

    add check condition to handle different exception

--- spring-boot-deep/src/main/java/io/sample/springbootdeep/controller/BookController.java
+++ spring-boot-deep/src/main/java/io/sample/springbootdeep/controller/BookController.java
@@ -1,15 +1,16 @@
 package io.sample.springbootdeep.controller;
 
 import io.sample.springbootdeep.domain.Book;
-import io.sample.springbootdeep.exception.BookNotFoundException;
 import io.sample.springbootdeep.service.BookService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @Controller
@@ -26,23 +27,24 @@ public class BookController {
     @GetMapping("/{id}")
     public String getBookById(@PathVariable Long id, Model model) {
         Book book = bookService.getBookById(id);
-        if (book == null) {
-            throw new BookNotFoundException("书单信息不存在");
-        }
         model.addAttribute("book", book);
         return "book";
     }
     
-//    @ExceptionHandler({Exception.class})
-//    public ModelAndView handleException(HttpServletRequest request, Exception e) {
-//
-//        log.error("Request URL: {}, Exception: {}", request.getRequestURL(), e.getMessage());
-//
-//        ModelAndView view = new ModelAndView();
-//        view.addObject("url", request.getRequestURL());
-//        view.addObject("exception", e);
-//        view.setViewName("error/error");
-//
-//        return view;
-//    }
+    @ExceptionHandler({Exception.class})
+    public ModelAndView handleException(HttpServletRequest request, Exception e) throws Exception {
+        
+        log.error("Request URL: {}, Exception: {}", request.getRequestURL(), e.getMessage());
+        
+        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
+            throw e;
+        }
+        
+        ModelAndView view = new ModelAndView();
+        view.addObject("url", request.getRequestURL());
+        view.addObject("exception", e);
+        view.setViewName("error/error");
+        
+        return view;
+    }
 }
