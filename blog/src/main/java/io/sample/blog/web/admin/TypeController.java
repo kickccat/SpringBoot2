169b03899ac307ee6cd606f83a2e8a390a69a098
Yi Zhou <kickccat@hotmail.com>
2018-09-11 16:46:52 +0200

    update type controller

--- blog/src/main/java/io/sample/blog/web/admin/TypeController.java
+++ blog/src/main/java/io/sample/blog/web/admin/TypeController.java
@@ -1,5 +1,6 @@
 package io.sample.blog.web.admin;
 
+import io.sample.blog.model.Type;
 import io.sample.blog.service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,14 +24,25 @@ public class TypeController {
     }
     
     @GetMapping("/types")
-    public String types(@PageableDefault(size = 10, sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable, Model model) {
+    public String types(@PageableDefault(size = 5, sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable, Model model) {
         
         model.addAttribute("page", typeService.listType(pageable));
         return "admin/types";
     }
     
     @GetMapping("/types/addType")
-    public String addType() {
+    public String add() {
         return "admin/type-add";
+    }
+    
+    @PostMapping("/types")
+    public String post(Type type) {
+        Type t = typeService.saveType(type);
+        if (t == null) {
+        
+        } else {
+        
+        }
+        return "redirect:/admin/types";
     }
 }
