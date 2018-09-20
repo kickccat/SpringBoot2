ad2871874d2bdd744e14b36dbde31ebc1cb0b242
Yi Zhou <kickccat@hotmail.com>
2018-09-20 16:16:47 +0200

    add aboutMe site and messages properties configuration

---
+++ blog/src/main/java/io/sample/blog/web/AboutMeController.java
@@ -0,0 +1,13 @@
+package io.sample.blog.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AboutMeController {
+    
+    @GetMapping("/aboutMe")
+    public String aboutMe() {
+        return "aboutMe";
+    }
+}
